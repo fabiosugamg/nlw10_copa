02e695d2ccfa8d32a2bd29758526d8796157cf88
fabiosugamg <fabiosugamg@gmail.com>
2022-11-07 09:50:51 -0300

    finalizacao nlw

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-function createGame() {
+function createGame(player1, hour, player2) {
     return `
-    <li>
-        <img src="./assets/icon_brazil.svg" alt="Bandeira do Brasil">
-        <strong>16:00</strong>
-        <img src="./assets/icon_serbia.svg" alt="Bandeira da Servia">        
-    </li>
+      <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      </li>
     `
-}
-
-function createCard(date, day, games) {
+  }
+  
+  let delay = -0.4;
+  function createCard(date, day, games) {
+    delay = delay + 0.4;
     return `
-    <div class="card">
+      <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
-          ${games}  
+          ${games}
         </ul>
-    </div>
+      </div>
     `
-}
-
-
-
-document.querySelector("#app").innerHTML = `
-<header>
-    <img src="./assets/logo.svg" alt="Logo da NLW">
-</header>
-<main id="cards">
-    ${createCard("24/11", "quinta", createGame())}
-    ${createCard("28/11", "segunda", createGame())} 
-    ${createCard("02/12", "sexta", createGame())}   
-</main>
-`
+  }
+  
+  document.querySelector("#cards").innerHTML =
+    createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+    createCard(
+      "28/11",
+      "segunda",
+      createGame("switzerland", "13:00", "brazil") +
+        createGame("portugal", "16:00", "uruguai")
+    ) +
+    createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  
